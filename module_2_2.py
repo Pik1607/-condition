c762b8c38c92d981b0135c5e729011406e69b479
Pik1607 <om_pikun@ros-stroi.com>
2024-11-13 23:01:05 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,20 @@
+#1
+first=16
+second=7
+third=1988
+if first==second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+else:
+    print(0)
+#2
+first=1988
+second=1607
+third=1988
+if first==second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+else:
+    print(0)
